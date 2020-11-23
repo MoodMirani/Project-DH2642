a2c510882d335d37073bdb961e0a9273ad79f6df
Nicole <66645928+nicoleno@users.noreply.github.com>
2020-11-23 16:00:04 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./Player";
 import { useDataLayerValue } from "./DataLayer";
+import TestPlayer from "./testHOME";
 
 const spotify = new SpotifyWebApi();
+console.log("här", spotify);
+console.log("Där", spotify.getTrack("6rqhFgbbKwnb9MLmUQDhG6"));
+
 
 function App() {
   const [{ user, token }, dispatch] = useDataLayerValue();
@@ -15,7 +19,7 @@ function App() {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
-    if (_token) {
+    if (_token) { // token är typ en "nyckel" från användaren
       dispatch({
         type: "SET_TOKEN",
         token: _token,
@@ -45,9 +49,20 @@ function App() {
 
   return (
     <div className="App">
-      {token ? <Player spotify={spotify} /> : <Login />}
+      {token ? <TestPlayer /> : <Login />}
     </div>
   );
 }
 
 export default App;
+
+
+/*
+
+return (
+    <div className="App">
+      {token ? <Player spotify={spotify} /> : <Login />}
+    </div>
+  );
+
+  */
---
+++ src/testHOME.js
@@ -0,0 +1,12 @@
+//testHOME.js
+
+import React from "react";
+import "./testHOME";
+
+
+function TestPlayer({ spotify }) {
+  return (<h1>Hello :D</h1>
+  );
+}
+
+export default TestPlayer;
