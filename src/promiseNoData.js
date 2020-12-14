463a140c72f81a22ab356a4cda205d6467432eaa
Amanda Carp <amandacarp@Amandas-MacBook-Air.local>
2020-12-14 19:52:35 +0100

    nothing new

--- src/promiseNoData.js
+++ src/promiseNoData.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Loading from './assets/loading.gif';
 function promiseNoData(promise, data, error, h){  
-    return  (!promise && "no data")     // case "0"  
-          || (error && <h1>error</h1>)   // case 3 
-          || (!data && <img src={Loading} alt=""/>)  // case 1
+    return  (!promise && "no data")  
+          || (error && <h1>error</h1>)   //HÄR KAN MAN HA ETT ALERT MEDDELANDE ISTÄLLET alert("Please choose a category!")
+          || (!data && <img src={Loading} alt=""/>)
 }
 export default promiseNoData;
