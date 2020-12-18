73ed52a6f32edc50efcd703a09fe304bd05dae41
Mahmoud Sherzad <47563174+MoodSherzad@users.noreply.github.com>
2020-12-18 04:07:42 +0100

    Update likedmusicView.js

--- src/view/likedmusicView.js
+++ src/view/likedmusicView.js
@@ -5,12 +5,16 @@ import "../css/AlbumView.css";
 
 
 const LikedmusicView = ({currentArtist, likedSongs}) => {
+    console.log(likedSongs)
+    likedSongs = likedSongs.flat()
+    const tracks = likedSongs.map(track => track.artists[0].name + " " + track.name)
+    console.log(tracks)
     return(
     <div className="trackContainer">
         
          <div><Link to="/"> <button className ="button">Back to search</button></Link>
             <div>
-                <h1 className="loadedTrack" >{likedSongs}</h1>
+                <h1 className="loadedTrack" >{tracks}</h1>
             </div>
         </div>
     </div>
