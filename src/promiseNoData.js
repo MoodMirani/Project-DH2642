dfb3fef2dcc7fb9a456fd1d68ad98b21062277a4
cdexwik <carolinadexwik@gmail.com>
2020-12-20 18:27:53 +0100

    Update on handle error

--- src/promiseNoData.js
+++ src/promiseNoData.js
@@ -3,8 +3,11 @@ import Loading from './assets/spinner-big.gif';
 import "./css/popify.css"
 
 function promiseNoData(promise, data, error) {
+    console.log(data, error)
+    console.log()
     return (!promise && "no data")
-        || (error && <h1>You need to type something in the searchbar</h1>)
+        || ((error || data === undefined) && <h1>You need to type something in the searchbar</h1>)
+        || ((!Array.isArray(data) || !data.length) && data && <h1>Unvalid search term, try again!</h1> )
         || (!data && <img className="centrePROM" src={Loading} alt="" />)
 
 }
